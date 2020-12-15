c2c6be3c257193cfb04a0a48ec0f288c29b028cb
PashaEfimchik <61446424+PashaEfimchik@users.noreply.github.com>
2020-12-15 21:50:24 +0300

    json options designer

---
+++ Lab_4/Options/JSONOptions.Designer.cs
@@ -0,0 +1,23 @@
+namespace FileWatcherService
+{
+    partial class JsonOptions
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+        }
+
+        #endregion
+    }
+}
