cd722fe482b44f238f5e23956b61ca11d7396912
PashaEfimchik <61446424+PashaEfimchik@users.noreply.github.com>
2020-12-15 21:38:04 +0300

    config manager designer

---
+++ Lab_4/ConfigurationManager.Designer.cs
@@ -0,0 +1,22 @@
+namespace FileWatcherService
+{
+    partial class ConfigurationManager
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+        }
+
+        #endregion
+    }
+}
