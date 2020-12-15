7bdc1324507845fc4e7813e2041d5d42a2fd526e
PashaEfimchik <61446424+PashaEfimchik@users.noreply.github.com>
2020-12-15 21:47:29 +0300

    installer1 designer

---
+++ Lab_4/Installer1.Designer.cs
@@ -0,0 +1,21 @@
+namespace FileWatcherService
+{
+    partial class Installer1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+        }
+    }
+}
